66a0e51fbf99fb3ebd3f6119c2703b4159a1f3e9
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2021-08-02 11:33:37 +0300

    1. Added more help-info; 2. Whenever unknown "model/modification" found with PlayerId - application will skip this record with corresponding log line.

--- PSA.Saver/Program.cs
+++ PSA.Saver/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -16,7 +17,12 @@ namespace PSA.Saver
         static void Main(string[] args)
         {
             Console.WriteLine("Pacific Storm - Allies gamesave correction unitily by VG.");
-            Console.WriteLine("Special for Samurai Sprit community :)");
+
+            if (args.Length == 0)
+            {
+                PrintHelpAndInfo();
+                return;
+            }
 
             try
             {
@@ -24,6 +30,12 @@ namespace PSA.Saver
                 MergeUnitMaps(PlayerIdMap, patch);
 
                 var file = new FileInfo(args[0]);
+                if (!file.Exists)
+                {
+                    Console.WriteLine("Specified file not found : {0}.", file.FullName);
+                    return;
+                }
+
                 var doc = Load(file);
                 var elements = FindPlayerIdElements(GetTacticLevelModule(doc)).ToList();
 
@@ -42,28 +54,64 @@ namespace PSA.Saver
                     }
                     if (modification != null)
                     {
+                        var correctedPlayerId = DeterminePlayerId(modification);
+
                         var oldValue = item.Attribute("value").Value;
-                        item.Attribute("value").Value = DeterminePlayerId(modification).ToString();
-                        Console.WriteLine("Fixed {0} record: {1} -> {2}", modification, oldValue, DeterminePlayerId(modification));
+
+                        if (correctedPlayerId.HasValue)
+                        {
+                            item.Attribute("value").Value = correctedPlayerId.ToString();
+                            Console.WriteLine("Fixed {0} record: {1} -> {2}", modification, oldValue, DeterminePlayerId(modification));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid PlayerId found but no 'model->id' match found in unit map.");
+                            Console.WriteLine("Consider adding new record to 'unit_map.csv' file for the following modification '{0}'.", modification);
+                        }
                     }
                 }
 
-                Save(doc, new FileInfo(Path.Combine(file.Directory.FullName,
-                    Path.GetFileNameWithoutExtension(file.Name) + "_" + file.Extension)));
+                var modified = new FileInfo(Path.Combine(file.Directory.FullName,
+                    Path.GetFileNameWithoutExtension(file.Name) + "_" + file.Extension));
+
+                Save(doc, modified);
+
+                Console.WriteLine("Completed. Modifed gamesave file : {0}", modified.FullName);
             }
             catch (Exception e)
             {
+                Console.WriteLine("Error occured. Error information will be presented below.");
+                Console.WriteLine("Contact the author if you need help.");
                 Console.WriteLine(e);
             }
         }
 
-        public static int DeterminePlayerId(string modification)
+        private static void PrintHelpAndInfo()
+        {
+            Console.WriteLine("In order to use this utility specify file path for your Pacific Storm gamesave.");
+            Console.WriteLine("Example: {0} \"{1}\".", Assembly.GetExecutingAssembly().GetName().Name, @"D:\Games\PSA\saves\save1.xml");
+            Console.WriteLine("Please note that the file path should be wrapped in quotation marks");
+            Console.WriteLine("(unless file path does not contain any white spaces).");
+            Console.WriteLine();
+            Console.WriteLine("Version : {0}", Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("Author  : Vladimir Gordiychuk ( https://vk.com/vladimir.gordiychuk )");
+            Console.WriteLine("Special thanks to Samurai Sprit community :)");
+        }
+
+        public static int? DeterminePlayerId(string modification)
         {
             var model = modification.Substring(
                 modification.IndexOf('.') + 1,
                 modification.LastIndexOf('.') - modification.IndexOf('.') - 1);
 
-            return PlayerIdMap[model];
+            if (PlayerIdMap.ContainsKey(model))
+            {
+                return PlayerIdMap[model];
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public static XElement GetTacticLevelModule(XDocument document)
